0769cb628a7dd0c8ed2d1733f2d0bc54e1fe20e4
Vitor Ferreira <victor-bash2@hotmail.com>
2024-10-04 18:52:04 -0300

    adding exercise 1

---
+++ chapter_1/binary_search/exercise_1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+
+namespace exercise_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercício para procurar determinado numero em um array");
+            Console.WriteLine();
+
+            int[] numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+            int searchValue = 10;
+
+            Console.WriteLine($"Numero a ser procurado: {searchValue}");
+
+            int low = 0;
+            int high = numbersArray.Length - 1;
+            int attempts = 1;
+            var sw = new Stopwatch();
+
+            sw.Start();
+
+            while(low <= high)
+            {
+                int middle = (low + high) / 2;
+                int guess = numbersArray[middle];
+
+                if(guess == searchValue)
+                {
+                    sw.Stop();
+                    Console.WriteLine($"Numero encontrado na posição {middle}, tempo decorrido: {sw.Elapsed}, tentativas: {attempts}");
+                    return;
+                }
+
+                if(guess < searchValue)
+                {
+                    low = middle + 1;
+                }
+                else
+                {
+                    high = middle - 1;
+                }
+
+                attempts++;
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Numero não encontrado, tempo decorrido: {sw.Elapsed}, tentativas: {attempts}");
+        }
+    }
+}
