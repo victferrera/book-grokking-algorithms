f1a6700ee0c4691282ddd4dd76f096ae381f2428
Vitor Ferreira <victor-bash2@hotmail.com>
2024-10-05 12:33:31 -0300

    LeetCode binary search problem 35

---
+++ chapter_1/binary_search/leetcode_binary_search_35/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace leetcode_binary_search_35
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution lc = new();
+
+            int res = lc.SearchInsert(new int[4] { 1, 3, 4, 5 }, 6);
+
+            Console.WriteLine(res);
+        }
+    }
+
+    public class Solution
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            int low = 0;
+            int high = nums.Length - 1;
+
+            while (low <= high)
+            {
+                int middle = low + (high - low) / 2;
+
+                if (nums[middle] == target)
+                    return middle;
+
+                if (nums[middle] < target)
+                    low = middle + 1;
+                else
+                    high = middle - 1;
+            }
+
+            return low;
+        }
+    }
+}
